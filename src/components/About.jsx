2bc47eb149248eae4328c9ff259b8373f9fb0148
Abhinav Gaur <abhinavajgaur@gmail.com>
2025-02-15 20:24:54 +0530

    About section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-25 md:my-10'>
+    <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-25 md:my-30'>
       <div className='sm:my-10'>
         <h1 className='text-center font-bold md:text-9xl text-5xl text-amber-50'>REDEFINING HUMAN </h1>
         <h1 className='text-center font-bold md:text-9xl text-5xl md:mr-1 mr-3 text-amber-50'>ABILITIES</h1>
@@ -10,7 +10,7 @@ const About = () => {
         </p>
       </div>
       <div className="flex items-center justify-center ">
-      <button className="relative px-6 py-3 my-3 text-gray-800 bg-yellow-200 rounded-full text-lg font-medium shadow-[0_0_20px_rgba(255,255,150,0.6)] hover:shadow-[0_0_30px_rgba(255,255,150,0.9)] transition-all z-[-1]">
+      <button className="relative px-8 py-3 my-3 text-gray-800 bg-yellow-200 rounded-full text-lg font-medium shadow-[0_0_20px_rgba(255,255,150,0.6)] hover:shadow-[0_0_30px_rgba(255,255,150,0.9)] transition-all z-[-1]">
         Join waitlist →
       </button>
     </div>
