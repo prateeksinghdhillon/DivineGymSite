12ee1dc51ebd164862c53d39383ff6960a6950dd
Abhinav Gaur <abhinavajgaur@gmail.com>
2025-02-21 18:36:08 +0530

    About section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <>
-    <div className=' max-w-screen-2xl container mx-auto px-4 md:px-20 my-25 md:my-30'>
-      <div className='sm:my-10'>
-        <h1 className='geist-font-500 text-center font-bold md:text-9xl md:justify-center text-5xl text-amber-50 '>REDEFINE HUMAN </h1>
-        <h1 className='geist-font-500 text-center font-bold md:text-9xl text-5xl md:mr-1 text-amber-50'>ABILITIES</h1>
-        <p className='geist-font-300 text-center text-2xl md:my-10 mt-10  text-amber-50'>The world's most advanced performance optimization <br />lab combining elite endurance training, longevity science,<br />and personalized health analytics.
+    <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-10 md:my-20">
+      <div className="sm:my-10">
+        <h1 className="geist-font-500 text-center font-bold text-4xl md:text-9xl text-amber-50 leading-tight">
+          REDEFINE HUMAN
+        </h1>
+        <h1 className="geist-font-500 text-center font-bold text-4xl md:text-9xl text-amber-50">
+          ABILITIES
+        </h1>
+        <p className="geist-font-300 text-center text-lg md:text-3xl mt-6 md:mt-10 text-amber-50 leading-relaxed">
+          The world's most advanced performance optimization <br /> lab combining elite endurance training, longevity science,<br /> and personalized health analytics.
         </p>
       </div>
-      <div className="flex items-center justify-center ">
-      <button className="geist-font-500 relative px-8 py-3 my-3 mt-30 text-gray-800 bg-yellow-200 rounded-full text-lg font-medium shadow-[0_0_20px_rgba(255,255,150,0.6)] hover:shadow-[0_0_30px_rgba(255,255,150,0.9)] transition-all z-[1] cursor-pointer">
-        Join waitlist →
-      </button>
+      <div className="flex items-center justify-center mt-6">
+        <button className="geist-font-500 px-6 py-2 md:px-8 md:py-3 bg-yellow-200 text-gray-800 rounded-full text-lg md:text-xl font-medium shadow-lg hover:shadow-xl transition-all">
+          Join waitlist →
+        </button>
+      </div>
     </div>
-    </div>
-    </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
